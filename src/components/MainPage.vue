20dbe809c199453be6491e66ab497a3b38ee4597
Ana Elisa Rodrigues do Couto <anaecouto@outlook.com>
2023-11-21 14:52:44 -0300

    first changes

---
+++ src/components/MainPage.vue
@@ -0,0 +1,80 @@
+<template>
+  <section>
+    <h2>História</h2>
+    <p>
+      O Clã Vortex surgiu como uma ideia forjada pela amizade improvável de dois pioneiros. Cada membro é acolhido com
+      calor e respeito, independentemente de sua experiência no jogo. A missão do clã é clara: construir uma comunidade
+      onde a diversão e o respeito são os pilares fundamentais.
+    </p>
+  </section>
+  <section>
+    <h2>Como o clã funciona</h2>
+    <p>
+      Bem-vindo ao nosso clã no emocionante mundo do recém-lançado MMORPG Throne And Liberty! Nosso clã é dedicado a
+      oferecer uma experiência de jogo envolvente e descontraída, embora também abracemos a competitividade. Valorizamos a
+      diversidade e damos as boas-vindas a jogadores de todas as habilidades. <br><br>
+
+      Estamos comprometidos em criar uma comunidade acolhedora e inclusiva, onde a diversão e o senso de cooperação são
+      tão importantes quanto a busca pela vitória. Aceitamos membros com mais de 18 anos, garantindo um ambiente maduro e
+      respeitoso para todos. <br><br>
+
+      Seja você é um jogador casual ou alguém que busca desafios competitivos, você encontrará seu lugar em nosso clã.
+      Junte-se a nós e embarque nesta jornada épica em Throne And Liberty.
+    </p>
+  </section>
+  <section>
+    <h2>Discord</h2>
+    <p>
+      Como qualquer boa comunidade, utilizamos o discord para nosso meio de comunicação. A entrada é obrigatória a TODOS
+      os membros. Segue o convite clicando no ícone abaixo:<br><br>
+      <a href="https://discord.gg/hUPGBGbjMv">
+        <img src="https://static-00.iconduck.com/assets.00/discord-icon-2048x2048-nnt62s2u.png" alt="Ícone do Discord"
+          style="width:50px; height:50px;">
+      </a><br><br>
+      <b>IMPORTANTE!!!!</b> A leitura das regras é imprescindível para que possamos ter um controle efetivo dos membros e
+      do servidor. Não se esqueçam que nós não recebemos nenhuma bonificação ou gratificação extra pelo gerenciamento de
+      todas as plataformas nas quais estamos inseridos. Portanto, tenha consideração pelos fundadores e demais
+      administradores do nosso clã!
+    </p>
+  </section>
+  <div class="copyright">
+    <p>&copy; 2023 Clã Vortex - All Rights Reserved</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #473dff;
+}
+
+h2 {
+  color: #ffffff;
+}
+
+p {
+  color: #7682ee;
+}
+
+.icon {
+  font-size: 24px;
+}
+
+.copyright {
+  margin-top: auto;
+  text-align: center;
+}
+</style>
+
+<script>
+
+</script>
